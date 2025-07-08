e4260bf4d7d6ab7fad13ac301152d4df87f0ce22
noahpessell <nwpessell@mail.com>
2025-07-07 22:33:57 -0400

    Part A completed

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+//  Counts from 0-30 by 1
+//        for (int cnt = 0; cnt <= 30; cnt++) {
+//            System.out.print(" " + cnt + " ");
+//        }
+//  Counts from 30-0 by 1
+//        for (int cnt = 30; cnt >= 0; cnt--) {
+//            System.out.print(" " + cnt + " ");
+//        }
+//  Counts from 0-18 by 3
+//        for (int cnt = 0; cnt <= 18; cnt = cnt + 3) {
+//            System.out.print(" " + cnt + " ");
+//        }
+//  Counts from 10-0 by 2
+//        for (int cnt = 10; cnt >= 0; cnt = cnt - 2) {
+//            System.out.print(" " + cnt + " ");
+//        }
+    }
+}
