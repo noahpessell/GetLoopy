2c1629de6f74cfc85b6c09c83730bdc30467a0ce
noahpessell <nwpessell@mail.com>
2025-07-08 00:54:06 -0400

    DieRoller Completed

--- src/DieRoller.java
+++ src/DieRoller.java
@@ -3,38 +3,49 @@ import java.util.Scanner;
 public class DieRoller {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean done = false;
         Random rnd = new Random();
         int die1, die2, die3, dieRoll;
         int rollCount = 1;
+        final String ROLL = "Roll";
+        final String DIE1 = "Die1";
+        final String DIE2 = "Die2";
+        final String DIE3 = "Die3";
+        final String SUM = "Sum";
         String playAgain = "";
         String trash = "";
-        System.out.println("Roll Die1 Die2 Die3 Sum");
-        do
-        {
+        boolean repeat = false;
+        boolean done = false;
+        System.out.printf("%6s %6s %6s %6s %6s\n\n", ROLL, DIE1, DIE2, DIE3, SUM);
+        do {
             die1 = rnd.nextInt(6) + 1;
             die2 = rnd.nextInt(6) + 1;
             die3 = rnd.nextInt(6) + 1;
-            while(die1 != die3 || die2 != die3)
-            {
+            dieRoll = die1 + die2 + die3;
+            while (die1 != die3 || die2 != die3) {
+                System.out.printf("%6d %6d %6d %6d %6d\n", rollCount, die1, die2, die3, dieRoll);
+                die1 = rnd.nextInt(6) + 1;
+                die2 = rnd.nextInt(6) + 1;
+                die3 = rnd.nextInt(6) + 1;
                 dieRoll = die1 + die2 + die3;
-                System.out.printf("%3d %3d %3d %3d %5d\n", rollCount, die1, die2, die3, dieRoll);
+                rollCount ++;
             }
-            System.out.print("Do you want to run it again? [Y/N] ");
-            playAgain = in.nextLine();
-            if (playAgain.equalsIgnoreCase("Y")) {
-            done = false;
-            repeat = false;
-        } else if (playAgain.equalsIgnoreCase("N")) {
-            System.out.println("Thanks for playing!");
-            done = true;
-            repeat = false;
-        } else {
-            trash = playAgain;
-            System.out.println(trash + " is not a valid response.");
-            done = false;
-            repeat = true;
-            done = true
+            do {
+                System.out.print("Do you want to continue throwing dice? [Y/N] ");
+                playAgain = in.nextLine();
+                if (playAgain.equalsIgnoreCase("Y")) {
+                    done = false;
+                    repeat = false;
+                } else if (playAgain.equalsIgnoreCase("N")) {
+                    System.out.println("Thanks for playing!");
+                    done = true;
+                    repeat = false;
+                } else {
+                    trash = playAgain;
+                    System.out.println(trash + " is not a valid response.");
+                    done = false;
+                    repeat = true;
+                }
+            } while(!done && repeat);
         } while(!done);
     }
 }
